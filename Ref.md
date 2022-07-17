22f17e6bd10c2e333ac1e9719b7748f2ceab8a95
pea-sys <pea98258@gmail.com>
2022-07-17 18:02:12 +0900

    forward

--- Ref.md
+++ Ref.md
@@ -1,3 +1,7 @@
 --[【C++】ヘッダファイルと cpp ファイルの事故らない扱い方(2/2)](https://nut-softwaredevelopper.hatenablog.com/entry/2017/04/04/143808)
 
 --[C/C++のビルドの仕組みとライブラリ](https://kamino.hatenablog.com/entry/c%2B%2B-principle-of-build-library)
+
+--[C++のムーブと完全転送を知る](https://proc-cpuinfo.fixstars.com/2016/03/c-html/)
+
+--[C++20スマートポインタ入門](https://qiita.com/hmito/items/9b35a2438a8b8ee4b5af)
---
+++ forward/forward/forward.cpp
@@ -0,0 +1,35 @@
+﻿// forward : 関数テンプレートの引数を転送する（実際はキャスト処理するだけ)
+
+//　コピーを抑止する場合に使用されることが多い？
+
+//この関数に渡した引数は、多くのケースにおいて右辺値参照にキャストされ、
+//ムーブされることに注意すること。同じ引数に対して二回以上関数を呼び出しを行いたい場合は、
+//std::forward()は使ってはならない：
+#include <iostream>
+#include <memory>
+#include <utility>
+
+/*
+shared_ptrは、指定されたリソースへの所有権(ownership)を共有(share)するスマートポインタである。
+複数のshared_ptrオブジェクトが同じリソースを共有し、所有者が0人、
+つまりどのshared_ptrオブジェクトからもリソースが参照されなくなると、リソースが自動的に解放される。
+*/
+template <class T, class A1, class A2>
+std::shared_ptr<T> factory(A1&& a1, A2&& a2) {
+	return std::shared_ptr<T>(new T(std::forward<A1>(a1), std::forward<A2>(a2)));
+}
+
+struct A {
+	A(int&, const double&) {}
+};
+
+int main()
+{
+	//std::shared_ptr<A> sp1 = factory<A>(2, 1.414);  // error: 2 will not bind to int&
+
+	int i = 2;
+	
+	std::shared_ptr<A> sp2 = factory<A>(i, 1.414);  // OK
+	std::cout << sp2 ;
+	std::cin.get();
+}
